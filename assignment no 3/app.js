b1c49da1ee8760e10fb976b601b13f1cbee1a3e6
Abdul Samad <abdulsamadborn03@gmail.com>
2021-03-31 16:28:22 +0500

    Add files via upload

---
+++ assignment no 3/app.js
@@ -0,0 +1,26 @@
+// const arr = [1,40,2,10,10,40,2,2,5,3,54,2,42,5,5,5]
+// arr.sort(function(a,b){return a-b});
+// const newSet = new Set(arr)
+
+// for (const i in arr) {
+//     console.log(newSet)
+// }
+
+// var i , j;
+
+// for(i=1;i<=5;i++){
+//    for(j=1;j<=i;j++){
+//         document.write(j+"&nbsp;&nbsp;&nbsp;");
+//    }
+//    document.write("<br>")
+// }
+
+// let num = prompt("enter")
+
+// let sum = 0;
+
+// while(num != 0) {
+//     sum += num % 10;
+//     num = parseInt(num/10)
+// }
+// console.log(sum)
